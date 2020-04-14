23bc742c9ebca518a36c886ad7dfaff659dfed6e
Johnathan Krueger <johnathankrueger@unknown3cf86205b685.attlocal.net>
2020-04-13 22:29:30 -0400

    changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,20 @@
-import { Module } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { LocalStrategy } from "./local.strategy";
-import { PassportModule } from "@nestjs/passport";
-import { JwtModule } from "@nestjs/jwt";
-import { UserModule } from "src/user/user.module";
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [
-    UserModule,
-    PassportModule,
-    JwtModule.register({
-      secret: "OWEI-492JGF03-34%$#&&PAQF2#%SH%^HJE^UJW$",
-      signOptions: { expiresIn: "60s" },
-    }),
-  ],
-  providers: [AuthService, LocalStrategy],
+	imports: [
+		UserModule,
+		PassportModule,
+		JwtModule.register({
+			secret: 'OWEI-492JGF03-34%$#&&PAQF2#%SH%^HJE^UJW$',
+			signOptions: { expiresIn: '60s' }
+		})
+	],
+	providers: [ AuthService, LocalStrategy ],
+	exports: [ AuthService ]
 })
 export class AuthModule {}
